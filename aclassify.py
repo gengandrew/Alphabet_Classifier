91cd84a24c914e24fce1bbffddf9115ff14508c9
Andrew Geng <gengandrew88@gmail.com>
2020-02-27 22:45:32 -0600

    Added epoch support

--- aclassify.py
+++ aclassify.py
@@ -1,9 +1,18 @@
 from emnist import extract_training_samples
 import matplotlib.pyplot as plt
 import numpy as np
+import sympy
 import math
 
-def getDensity(mean, covar, xtest):
+def getMaxConfidence(confidences):
+    max = (0,0)
+    for confidence in confidences:
+        if confidence[1] > max[1]:
+            max = confidence
+    
+    return max
+
+def getDensity(mean, covar, scalar, xtest):
     normX = xtest - mean
     iconvar = np.linalg.inv(covar)
     
@@ -20,6 +29,7 @@ def getDensity(mean, covar, xtest):
     return confidence
 
 
+bestscore = 0
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'p', 'w', 'x', 'y', 'z']
 images, labels = extract_training_samples('letters')
 
@@ -36,84 +46,130 @@ for image in images:
     for i in range(0,28):
         total = 0
         for j in range(0,28):
-            total = total + (image[i][j]/255)
+            total = total + (image[i][j])
         
-        parsed.append(total/28)
+        parsed.append(total)
 
     for i in range(0, 28):
         total = 0
         for j in range(0,28):
-            total = total + (image[j][i]/255)
+            total = total + (image[j][i])
         
-        parsed.append(total/28)
+        parsed.append(total)
 
     parsedSet.append(np.array(parsed))
     
-    if trainingSize > 2000:
+    if trainingSize > 4000:
         break
     else:
         trainingSize = trainingSize + 1
 
 
 print("Number of training set taken is " + str(len(parsedSet)) + " with shape " + str(parsedSet[0].shape))
-xtrains = parsedSet[500:len(parsedSet)]
-xtests = parsedSet[:len(parsedSet)-1500]
+xtrains = parsedSet[1000:len(parsedSet)]
+xtests = parsedSet[:1000]
 
-means = [None]*len(alphabet)
-counts = [None]*len(alphabet)
-for i in range(0,len(alphabet)):
-    means[i] = np.zeros((56,))
-    counts[i] = 0
+for epoch in range(0,500):
+    means = [None]*len(alphabet)
+    counts = [None]*len(alphabet)
+    for i in range(0,len(alphabet)):
+        means[i] = np.zeros((56,))
+        counts[i] = 0
 
-index = 0
-for xtrain in xtrains:
-    means[labels[index]-1] = means[labels[index]-1] + xtrain
-    counts[labels[index]-1] = counts[labels[index]-1] + 1
-    index = index + 1
+    index = 0
+    for xtrain in xtrains:
+        means[labels[index]-1] = means[labels[index]-1] + xtrain
+        counts[labels[index]-1] = counts[labels[index]-1] + 1
+        index = index + 1
 
-for i in range(0,len(alphabet)):
-    means[i] = means[i]/counts[i]
+    for i in range(0,len(alphabet)):
+        means[i] = means[i]/counts[i]
 
-# print(len(means))
-# for i in range(0,len(alphabet)):
-#     print(means[i])
+    # print(len(means))
+    # for i in range(0,len(alphabet)):
+    #     print(means[i])
 
-covars = [None]*len(alphabet)
-counts = [None]*len(alphabet)
-for i in range(0,len(alphabet)):
-    covars[i] = np.zeros((56,56))
-    counts[i] = 0
+    covars = [None]*len(alphabet)
+    counts = [None]*len(alphabet)
+    for i in range(0,len(alphabet)):
+        covars[i] = np.zeros((56,56))
+        counts[i] = 0
 
-index = 0
-for xtrain in xtrains:
-    label = labels[index]-1
-    temp = xtrain - means[label]
-    covars[label] = covars[label] + np.outer(temp,temp)
-    counts[label] = counts[label]+1
-    # if labels[index] == 1:
-    #     temp = xtrain - mean
-    #     covar = covar + np.outer(temp,temp)
-    #     count = count+1
-    index = index+1
+    index = 0
+    for xtrain in xtrains:
+        label = labels[index]-1
+        temp = xtrain - means[label]
+        covars[label] = covars[label] + np.outer(temp,temp)
+        counts[label] = counts[label]+1
+        # if labels[index] == 1:
+        #     temp = xtrain - mean
+        #     covar = covar + np.outer(temp,temp)
+        #     count = count+1
+        index = index+1
 
-for i in range(0,len(alphabet)):
-    covars[i] = covars[i]/counts[i]
+    for i in range(0,len(alphabet)):
+        covars[i] = covars[i]/counts[i]
 
-for i in range(0,len(alphabet)):
-    for row in range(0,56):
-        for col in range(0,56):
-            fuzz = np.random.normal(0,0.0001,1)[0]
-            covars[i][row][col] = covars[i][row][col]
+    for i in range(0,len(alphabet)):
+        # _, inds = sympy.Matrix(covars[i]).T.rref()
+        # while not len(inds) == 56:
+        for row in range(0,56):
+            for col in range(0,56):
+                fuzz = np.random.normal(0,0.0001,1)[0]
+                covars[i][row][col] = covars[i][row][col] + fuzz
+        
+            # _, inds = sympy.Matrix(covars[i]).T.rref()
+            # print("At stage " + str(i) + " and " + str(len(inds)))
 
-print("Starting to calculate the MVN density")
+    # print("Starting to calculate the MVN density")
 
-scalars = [None]*len(alphabet)
-for i in range(0,len(alphabet)):
-    scalars[i] = 1 / np.sqrt( pow((2*math.pi),56) * np.linalg.det(covars[i]))
-    print(scalars[i])
+    scalars = [None]*len(alphabet)
+    for i in range(0,len(alphabet)):
+        scalars[i] = 1 / np.sqrt( pow((2*math.pi),56) * abs(np.linalg.det(covars[i])))
+        # print(scalars[i])
 
+    epochscore = 0
+    for test in range(0,1000):
+        confidences = [None]*len(alphabet)
+        for i in range(0,len(alphabet)):
+            confidences[i] = (i, getDensity(means[i], covars[i], scalars[i], xtests[0]))
 
-# for test in range(0,500):
-#     confidence = getDensity(mean, covar, xtests[test])
-#     print("Confidence for " + str(test) + " is: " + str(confidence))
+        prediction = getMaxConfidence(confidences)
+        if prediction[0] == labels[test]-1:
+            epochscore = epochscore + 1
 
+    print("Epoch #" + str(epoch) + " with final score of " + str(epochscore))
+    if epochscore > bestscore:
+        bestscore = epochscore
+        covarfilename = "e" + str(epoch) + "s" + str(epochscore) + "[covars]" + ".txt"
+        meanfilename = "e" + str(epoch) + "s" + str(epochscore) + "[means]" + ".txt"
+
+        with open(covarfilename, 'w') as outfile:
+            outfile.write("# Covariance Matrices with Score " + str(epochscore) + "\n")
+            aindex = 0
+            for covar in covars:
+                outfile.write("# Covariance number " + str(aindex) + " letter " + str(alphabet[aindex]) + "\n")
+                np.savetxt(outfile, covar)
+                aindex = aindex + 1
+
+        with open(meanfilename, 'w') as outfile:
+            outfile.write("# Mean Matrices with Score " + str(epochscore) + "\n")
+            aindex = 0
+            for mean in means:
+                outfile.write("# Mean number " + str(aindex) + " letter " + str(alphabet[aindex]) + "\n")
+                np.savetxt(outfile, mean)
+                aindex = aindex + 1
+        
+
+# # Code for getting data out of file
+# new_data = np.loadtxt(covarfilename)
+# new_data = new_data.reshape((26, 56, 56))
+# assert(np.all(new_data == covars))
+
+# new_data = np.loadtxt(meanfilename)
+# new_data = new_data.reshape((26, 56))
+# assert(np.all(new_data == means))
+
+# plt.figure(1, figsize=(3, 3))
+# plt.imshow(images[0], cmap=plt.cm.gray_r, interpolation='nearest')
+# plt.show()
